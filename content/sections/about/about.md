8b325464fdb62971b419854fee47782c1fabc735
nayanadasgupta <nayana.dasgupta@outlook.com>
2022-10-29 21:55:00 +0100

    Add work experience

--- content/sections/about/about.md
+++ content/sections/about/about.md
@@ -5,7 +5,7 @@ imageAlt: "Handrawn image of a cat"
 
 Hello! My name is Nayana and I'm a Software Engineer at Twilio who loves all things code. I have a deep passion for building functional and performant products, with great user experiences and can often be found tinkering with various personal projects, contributing to open-source and experimenting with new technologies.
 
-I recently graduated with an MEng in Computer Science with First Class Honours from UCL, with my dissertation investigating how we can apply social mechanisms to multi-agent systems to enable the emergence of cooperation within groups of AI agents. 
+I recently graduated with an MEng in Computer Science with First Class Honours from University College London. In my dissertation, I researched techniques for developing Cooperative AI systems by applying social mechanisms to populations of reinforcement learning agents. 
 
 Outside of work, I love reading, playing board games and watching funny cat videos.
 
--- src/custom-sections/experience.js
+++ src/custom-sections/experience.js
@@ -17,25 +17,32 @@ const Date = styled.h3`
     font-size: 1em;   
 `
 
+const Link = styled.a`
+    color: #566763;
+`
+
 export default function ExperienceSection({heading, sectionId}) {
     return (
         <Animation type="fadeIn">
             <Section heading={heading} anchor={sectionId}>
                 <Container>
-                    <Heading>Software Engineer @ Twilio</Heading> 
+                    <Heading>Software Engineer @ <a href="https://www.twilio.com"><u>Twilio</u></a></Heading> 
                     <Date>August 2022 - Now</Date>
                 </Container>
                 <p></p>
                 <Container>
-                    <Heading>Software Engineer Intern @ Twilio</Heading> 
+                    <Heading>Software Engineer Intern @ <a href="https://www.twilio.com"><u>Twilio</u></a></Heading> 
                     <Date>June - August 2021</Date>
                 </Container>
-                <p></p>
+                <p>
+                    Built accessibility into Twilio Flex, after proposing a body of work to ensure a great experience for screen reader and keyboard-only users by following best practices. This involved adding new functionality and refactoring the entire codebase to ensure that Flex became fully keyboard-navigable, worked well with a screen reader and handled errors and notifications in an accessible way.
+                    I also refactored the Flex Diagnostics Plugin and updated its UI to meet Twilio Paste standards. This work involved close collaboration with UX design and product teams and led to the creation of accessibility guidelines for Flex. The tech stack used included  React, Redux, TypeScript, Jest, Cypress.
+                </p>
                 <Container>
-                    <Heading>Software Engineer Intern @ Publicis Sapient</Heading> 
+                    <Heading>Software Engineer Intern @ <a href="https://www.publicissapient.com/"><u>Publicis Sapient</u></a></Heading> 
                     <Date>June - August 2020</Date>
                 </Container>
-                <p></p>
+                <p>Developed a progressive web app that directs customers along the most efficient route through a Tesco store, based on the contents of their shopping list. The tech stack used included MongoDB, Node.js, Express and React and the project was deployed using Jenkins and Kubernetes.</p>
             </Section>
         </Animation>
     );
