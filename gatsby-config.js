75f0f54725c5d4272526b1ca3853f6a89094956a
nayanadasgupta <nayana.dasgupta@outlook.com>
2022-10-30 12:38:11 +0000

    Revert "Revert "Merge pull request #1 from nayanadasgupta/gatsby""
    This reverts commit ccc3b39fbe35f819bd09604556816405fa2d39ec.

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-theme-portfolio-minimal",
+      options: {
+        siteUrl: "https://nayanadasgupta.com", // Used for sitemap generation
+        manifestSettings: {
+          favicon: "./content/images/android-chrome-512x512.png", // Path is relative to the root
+          siteName: "Nayana Dasgupta · Portfolio", // Used in manifest.json
+          shortName: "Portfolio", // Used in manifest.json
+          startUrl: "/", // Used in manifest.json
+          backgroundColor: "#FFFFFF", // Used in manifest.json
+          themeColor: "#000000", // Used in manifest.json
+          display: "minimal-ui", // Used in manifest.json
+        },
+        contentDirectory: "./content",
+        blogSettings: {
+          path: "/blog", // Defines the slug for the blog listing page
+          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        },
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  AboutSection,
+  HeroSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+import ExperienceSection from "../custom-sections/experience";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Nayana Dasgupta" />
+      <Page useSplashScreenAnimation={false}>
+        <HeroSection sectionId="hero" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
+        <AboutSection sectionId="about" heading="About" />
+        {/* <InterestsSection sectionId="details" heading="Details" /> */}
+        <ExperienceSection sectionId="experience" heading="Experience" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        {/* <ProjectsSection sectionId="experience" heading="Experience" /> */}
+        {/* <ContactSection sectionId="github" heading="Contact Me" /> */}
+        {/* <Section heading="This is a brand new section.">
+              <p>You can add your custom JSX here.</p>
+        </Section> */}
+      </Page>
+    </>
+  );
+}
